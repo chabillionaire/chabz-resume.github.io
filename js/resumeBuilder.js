78f5e21b2be0d2aff8281246d7b6c4781be596fc
chabillionaire <marcusvbenjamin@gmail.com>
2015-10-18 17:28:09 -0600

    This is the resume project from Udacity. Project 2
    Let's get to work with Javascript

---
+++ js/resumeBuilder.js
@@ -0,0 +1,108 @@
+/*
+This is empty on purpose! Your code to build the resume will go here.
+ */
+ var bio = {
+    "name": "Marcus Benjamin",
+    "role": "Web Developer",
+    "contact": {
+        "mobile": "303-217-6261",
+        "email": "marcusvbenjamin@gmail.com",
+        "twitter": "@meutron",
+        "github": "chabillionaire",
+        "location": "Denver, CO"
+    },
+    "welcomeMessage": "Available and ready for front-end web development",
+    "skills": [
+        "web-development",
+        " teaching",
+        " applied linguistics",
+        " being a hype-man for Google products"
+    ],
+    "bioPic": "images/marcus.jpg"
+};
+
+var work : {
+    "currentjob": {
+        "position": "5th Grade English Teacher",
+        "employer": "International School of Denver",
+        "time": "2014 - Present",
+        "city": "Denver, CO"
+    },
+    "previous": [
+        {
+            "position": "English Language and Cultural Assistant",
+            "employer": "Colegio Enrique Gines",
+            "time": "2012 - 2014",
+            "city": "Frigiliana, Málaga, Spain"
+        },
+        {
+            "position": "English Language and Cultural Assistant",
+            "employer": "Colegio Valderrey",
+            "time": "2010-2012",
+            "city": "Algete, Madrid, Spain"
+        }
+    ]
+}
+var education = {
+    "schools": [
+        {
+            "name": "University of Southern California",
+            "graduation": "December 2014",
+            "degree": "Master of Arts in Teaching English to Speakers of Other Languages",
+            "location": "Los Angeles, California"
+        },
+        {
+            "name": "University of Oklahoma",
+            "graduation": "May 2009",
+            "degree": "Bachelors of Arts in Internation and Area Studies",
+            "minor": "history",
+            "location": "Norman, Oklahoma"
+        }
+    ]
+}
+
+var projects = {
+    "web-development": [
+        {
+            "name": "portfolio",
+            "link": "chabillionaire.github.io",
+            "objective": "This was a page created as part of my Udacity Front End Developer program in order to highlight projects I had completed for my nanodegree."
+        },
+        {
+            "name": "Gliding Vowel English",
+            "link": "http://www.glidingvowel.com",
+            "description": "This is an ongoing project to create an online, blended synchronous/asynchronous language school. So far it is using a combination of Moodle and BigBlueButton to achieve the goals. Status: currently in progress, not officially online."
+        }
+    ]
+}
+ /*var formattedName = HTMLheaderName.replace("%data%", bio.name);
+ var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+ var formattedMobile = HTMLmobile.replace("%data%", bio.contact.mobile);
+ var formattedEmail = HTMLemail.replace("%data%", bio.contact.email);
+ var formattedTwitter = HTMLtwitter.replace("%data%", bio.contact.twitter);
+ var formattedGithub = HTMLgithub.replace("%data%", bio.contact.github);
+ var formattedLocation = HTMLlocation.replace("%data%", bio.contact.location);
+ var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data", bio.welcomeMessage);
+ var formattedSkills = HTMLskills.replace("%data%", bio.skills);
+ var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
+
+
+ $("#header").prepend(formattedRole);
+ $("#header").prepend(formattedName);
+ $("#topContacts").append(formattedMobile);
+ $("#topContacts").append(formattedEmail);
+ $("#topContacts").append(formattedTwitter);
+ $("#topContacts").append(formattedGithub);
+ $("#topContacts").append(formattedLocation);
+ $("#header").append(formattedBioPic);
+ $("#header").append(HTMLskillsStart);
+ $("#header").append(formattedSkills);
+ $("#workExperience").append(HTMLworkStart);
+ $("#workExperience").append(work["position"]);
+ $("#education").append(HTMLschoolStart);
+ $("#education").append(education.name); */
+
+
+
+
+ 
